05c78348b0bbd871735da32a2a4e341979235b89
gregallentrester <gregallentrester@gmail.com>
2023-03-03 03:53:48 -0600

    Add files via upload

---
+++ simple/src/main/java/com/example/App.java
@@ -0,0 +1,18 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
+
+// https://github.com/spring-guides/tut-spring-boot-oauth2
+// https://github.com/spring-guides/tut-spring-boot-oauth2/blob/main/simple/README.adoc#github-application-config
+
+// Client ID | ef63474cb1e06a0278f3
+// Client Secret | 0fbc560d2753ff6fa440d8d87b1c70f2c1e16345 [ generated ]
